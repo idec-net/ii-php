b8e85ef58c11396243002c47263e06c7d3026907
vit <vityairk@gmail.com>
2016-02-23 14:37:59 +0800

    webfetch filter

--- webfetch.php
+++ webfetch.php
@@ -6,13 +6,6 @@ function getfile($add) {
 	echo "fetch ".$add."\n";
 	return file_get_contents($add);
 }
-function getLocalEcho($echo) {
-	$file=getecho($echo);
-	if (!$file) return [];
-
-	$arr=explode("\n", $file);
-	return $arr;
-}
 
 function parseFullEchoList($echobundle) {
 	$echos2d=array();
@@ -36,6 +29,8 @@ function parseFullEchoList($echobundle) {
 
 function fetch_messages($config, $one_request_limit=20, $fetch_limit=false, $xcenable=false) {
 	// xcenable - not implemented, fetch_limit - only N messages at time
+	global $access;
+
 	$echoesToFetch=array_slice($config,1);
 	$adress=$config[0];
 
@@ -43,10 +38,10 @@ function fetch_messages($config, $one_request_limit=20, $fetch_limit=false, $xce
 	($fetch_limit!=false) ? $bundleAdress.="/-".intval($fetch_limit).":".intval($fetch_limit) : false;
 
 	$echoBundle=getfile($bundleAdress);
-	$remoteEchos2d=parseFullEchoList(applyBlackList($echoBundle));
+	$remoteEchos2d=parseFullEchoList($access->applyBlackList($echoBundle));
 
 	foreach($echoesToFetch as $echo) {
-		$localMessages=getLocalEcho($echo);
+		$localMessages=$access->getMsgList($echo);
 		
 		$remoteMessages=$remoteEchos2d[$echo];
 
@@ -63,7 +58,7 @@ function fetch_messages($config, $one_request_limit=20, $fetch_limit=false, $xce
 				$arr=explode(":",$bundle);
 				if(!empty($arr[0])) {
 					$msgid=$arr[0]; $message=b64d($arr[1]);
-					$hash=savemsg($msgid, $echo, $message);
+					$hash=$access->saveMessage($msgid, $echo, $message, $raw=true);
 					if($hash) {
 						echo "message saved: ok\n";
 					}
